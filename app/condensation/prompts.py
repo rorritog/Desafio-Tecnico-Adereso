2d918bf627d59b3119ac26ba0de8dc9140c2986d
rorritog <rgalleguillos001@gmail.com>
2024-09-11 13:13:35 -0300

    aplica mejora: cantidad de fragmentos generados

--- app/condensation/prompts.py
+++ app/condensation/prompts.py
@@ -4,19 +4,23 @@ def get_system_prompt() -> str:
     """
     return """
         You are a data classification assistant for fragments of an article containing the documentation for an IT functionality.
-        Based on the fragments in spanish given by the user you are gonna generate an array of json objects where each element corresponds to each fragment.
+        You are gonna be provided with a number of fragments from an article in this format:
+        <fragment_number>:
+        <fragment_content>
+              
+        Based on the fragments in spanish given by the user you are gonna generate an array of json objects where each element corresponds to one fragment given. The number of fragments in the output array must be equal to the number of input fragments.
 
         Each fragment json must contain the following fields:
 
         title: An adequate title for the fragment. Base your criteria on describing what can be achieved with the content of the fragment.
         summary: A summary of the fragment based on its content. Avoid explaining what the fragment contains. 
-        tags: Tags relevant to the fragment. Use words related with the IT project itself.
+        tags: Array of Tags relevant to the fragment. Use words related with the IT project itself. Avoid using capital letters.
         article_url: the reference url of the article.
         content: the fragment itself.
 
-        Give only the array in json structure as an answer. 
+        Give only a json object with the key 'fragments' containing the array of json structured fragments as an answer.
         Use the exact fragment in the content field.
-        The json generated must be a valid json in the rfc 8259 specification
+        The json generated must be a valid json in the rfc 8259 specification.
     """
 
 
